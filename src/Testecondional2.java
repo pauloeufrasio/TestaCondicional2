431147a4fac8224848bd8b959b9cb241ae042b2a
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-02-20 16:49:06 -0300

    Initial commit

---
+++ src/Testecondional2.java
@@ -0,0 +1,19 @@
+public class Testecondional2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Testando condicionais ");
+        int idade = 20;
+        int quantidadePessoa = 3;
+        if ( idade >= 18) {
+            System.out.println("Voce tem mais de 18 anos "); // Testando condição
+        }
+        else if (quantidadePessoa >= 2){
+            System.out.println("Voce não tem 18, mais pode entrar, pois está acompanhado por maiores ");
+        }
+        else {
+            System.out.println("Infelizmente voce não pode entrar");
+        }
+    }
+    }
+

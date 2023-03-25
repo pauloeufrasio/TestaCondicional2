5f355d114e60ef03b8bd78c03045633fdf1f6af4
paulo <henriquepaulo0606@gmail.com>
2023-03-25 09:55:59 -0300

    Testando variavel do tipo boolean

--- src/Testecondional2.java
+++ src/Testecondional2.java
@@ -3,16 +3,19 @@ public class Testecondional2 {
             public static void main(String[] args) {
                 System.out.println("testando condicionais");
 
-                int idade = 20;
-                int quantidadePessoas = 3;
+                int idade = 15;
+                int quantidadePessoas = 1;
+                // Usando uma variavel do tipo boolean pra testar se está acompanhado ou não
                 boolean acompanhado;
 
+                // testando condição se a quantidadePessoas for maior que 2 a variavel acompanhado é verdadeiro
                 if (quantidadePessoas >= 2) {
                     acompanhado = true;
                 } else {
+                // Se nao a variavel acompanhado tem que me retornar falso
                     acompanhado = false;
                 }
-
+                // Testando condição se idade for maior que 18 ou acompanhado for verdadeiro ele pode acessar senão ele nao esta encaixado as regras do programa
                 if (idade >= 18 || acompanhado) {
 
                     System.out.println("Seja bem vindo");
